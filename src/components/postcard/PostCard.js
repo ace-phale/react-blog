59e81b1007848b103d7fd6d7688dd3147ad2b777
acephale <patryk.syka@gmail.com>
2021-02-26 07:01:08 +0100

    imporoved syling and mobile behaviour

--- src/components/postcard/PostCard.js
+++ src/components/postcard/PostCard.js
@@ -6,6 +6,9 @@ import LoadingCard from '../../components/loadingcard/LoadingCard';
 //react-bootstrap
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 //react-router-bootstrap
 import { LinkContainer } from 'react-router-bootstrap';
@@ -32,8 +35,8 @@ const PostCard = (props) => {
     </Container>
   ));
   return (
-    <Container className='col-12 col-lg-4 p-lg-4 pb-3'>
-      <Card style={{ height: '100%' }}>
+    <Container className='col-12 col-lg-4 p-lg-4 pb-3 '>
+      <Card className='shadow-sm hoverShadow' style={{ height: '100%' }}>
         {!imageIsLoaded && (
           <Container style={{ position: 'relative', paddingBottom: '56.2%' }}>
             <LoadingCard />
@@ -63,7 +66,21 @@ const PostCard = (props) => {
           </Card.Subtitle>
 
           <Card.Subtitle className='font-weight-bold small mt-2'>Likes: {likes}</Card.Subtitle>
-          <Card.Subtitle className='mt-1 flex-grow'>{displayTags}</Card.Subtitle>
+          <Row>
+            <Card.Subtitle className='col-7 mt-1 flex-grow'>{displayTags}</Card.Subtitle>
+            <Col className='' style={{ position: 'relative' }}>
+              <Link to={'/post/' + id}>
+                <Button
+                  className=''
+                  style={{ position: 'absolute', bottom: '0', left: '100%', transform: 'translate(-119%, 0)' }}
+                  size=''
+                  variant='outline-dark'
+                >
+                  See more
+                </Button>
+              </Link>
+            </Col>
+          </Row>
         </Card.Body>
       </Card>
     </Container>
