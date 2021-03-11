07e36e7c499c68b72e28c2421cad4cb0c0867068
acephale <patryk.syka@gmail.com>
2021-03-11 15:34:16 +0100

    implemented UserDetail view with users posts

--- src/components/userdetails/UserDetails.js
+++ src/components/userdetails/UserDetails.js
@@ -1,12 +1,38 @@
 import './UserDetails.css';
 
+//react-bootstrap
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 const UserDetails = (props) => {
-  const { dateOfBirth, email, firstName, gender, id, lastName, location, picture, title } = props.user;
+  const { dateOfBirth, email, firstName, gender, id, lastName, registerDate, location, picture, title } = props.user;
 
   return (
-    <div>
-      <img src={picture} />
-    </div>
+    <Container style={{ width: '80%' }} fluid>
+      <Row>
+        <Col className='col-12 mt-3'>
+          <Card>
+            <Container className='p-0' style={{ display: 'flex', flex: '1 1 auto' }}>
+              <Image src={picture} alt='Card image cap' />
+              <Card.Body>
+                <Card.Title className='mb-3'>
+                  {firstName} {lastName}
+                </Card.Title>
+                <Card.Text>
+                  {location.city}, {location.country}
+                </Card.Text>
+                <Card.Text>{email}</Card.Text>
+              </Card.Body>
+            </Container>
+            <Card.Footer>
+              <small className='text-muted'>Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/pages/userpage/UserPage.js
+++ src/pages/userpage/UserPage.js
@@ -4,8 +4,11 @@ import useGetData from '../../hooks/useGetData';
 //components
 import LoadingCard from '../../components/loadingcard/LoadingCard';
 import UserDetails from '../../components/userdetails/UserDetails';
+import PostCard from '../../components/postcard/PostCard';
 //react
 import { useEffect, useState } from 'react';
+//react-bootstrap
+import Row from 'react-bootstrap/Row';
 
 //react-router
 import { useParams } from 'react-router-dom';
@@ -13,8 +16,13 @@ import { useParams } from 'react-router-dom';
 const UserPage = () => {
   const { id } = useParams();
   const [userData, setUserData] = useState();
+  const [usersPosts, setUsersPosts] = useState();
   const { isLoading, isError, error, data, getData } = useGetData('user/' + id);
 
+  const { isLoading: postIsLoading, isError: postIsError, error: postError, data: postData, getData: getPostData } = useGetData(
+    'user/' + id + '/post'
+  );
+  //get user data
   useEffect(async () => {
     await getData();
   }, []);
@@ -22,11 +30,26 @@ const UserPage = () => {
     setUserData(data);
   }, [data]);
 
+  //get post data
+  useEffect(() => {
+    getPostData();
+  }, []);
+
+  useEffect(() => {
+    setUsersPosts(postData);
+  }, [postData]);
+
+  console.log(postData);
   if (isLoading || !data || !userData) {
     return <LoadingCard />;
   }
-  console.log(userData);
-  return <UserDetails user={userData} />;
+  const displayPosts = usersPosts.data.map((post) => <PostCard key={post.id} post={post} />);
+  return (
+    <>
+      <UserDetails user={userData} />
+      <Row className='mx-lg-3  m-1 mt-3 mt-md-0'>{displayPosts}</Row>
+    </>
+  );
 };
 
 export default UserPage;
